prdatur/emmylua-analyzer-rust

commit f7b400c71b80d69873d3286554b2e7fb9180b3b4

fix empty lua workspace issue

crates/emmylua_ls/src/handlers/initialized/mod.rs
@@ -168,6 +168,12 @@ pub async fn init_analysis(
         ProgressTask::LoadWorkspace,
         Some(String::from("Finished loading workspace files")),
     );
+    status_bar.update_progress_task(
+        client_id,
+        ProgressTask::LoadWorkspace,
+        Some(100),
+        Some("Indexing complete".to_string()),
+    );
 
     drop(mut_analysis);
 

commit b7dc4d6cfd22bc4ff5475328f866364c5c1348ba

fix(progress): don't finish before update
Problem: "Load workspace" progress is updated after finishing,
triggering errors in clients (e.g. Neovim).
(Regression introduced in f7b400c71b80d69873d3286554b2e7fb9180b3b4)

Solution: Correct sequencing of `update_progress` and `finish_progress`.

Also make `finish_progress` message for diagnostics consistent.

crates/emmylua_ls/src/context/file_diagnostic.rs
@@ -205,7 +205,7 @@ async fn workspace_diagnostic(
                     status_bar.finish_progress_task(
                         client_id,
                         ProgressTask::DiagnoseWorkspace,
-                        None,
+                        Some("Diagnosis complete".to_string()),
                     );
                     break;
                 }

crates/emmylua_ls/src/handlers/initialized/mod.rs
@@ -163,15 +163,15 @@ pub async fn init_analysis(
         mut_analysis.update_files_by_path(files);
     }
 
-    status_bar.finish_progress_task(
-        client_id,
-        ProgressTask::LoadWorkspace,
-        Some(String::from("Finished loading workspace files")),
-    );
     status_bar.update_progress_task(
         client_id,
         ProgressTask::LoadWorkspace,
-        Some(100),
+        None,
+        Some(String::from("Finished loading workspace files")),
+    );
+    status_bar.finish_progress_task(
+        client_id,
+        ProgressTask::LoadWorkspace,
         Some("Indexing complete".to_string()),
     );
 