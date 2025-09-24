c57fb4e83b8320fb7939e6629de3a4d333791a7f
CppCXY <812125110@qq.com>
2025-09-24 14:17:25 +0800

    remove export check

--- crates/emmylua_code_analysis/src/diagnostic/test/inject_field_test.rs
+++ crates/emmylua_code_analysis/src/diagnostic/test/inject_field_test.rs
@@ -151,63 +151,63 @@ mod test {
     //     ));
     // }
 
-    #[test]
-    fn test_export() {
-        let mut ws = VirtualWorkspace::new();
-        ws.def_file(
-            "a.lua",
-            r#"
-            ---@export
-            local export = {}
+    // #[test]
+    // fn test_export() {
+    //     let mut ws = VirtualWorkspace::new();
+    //     ws.def_file(
+    //         "a.lua",
+    //         r#"
+    //         ---@export
+    //         local export = {}
 
-            export.a = 1
+    //         export.a = 1
 
-            return export
-            "#,
-        );
-        assert!(!ws.check_code_for(
-            DiagnosticCode::InjectField,
-            r#"
-            local a = require("a")
-            a.newField = 1
-            "#,
-        ));
-        assert!(ws.check_code_for(
-            DiagnosticCode::InjectField,
-            r#"
-            local a = require("a")
-            a.a = 2
-            "#,
-        ));
-    }
+    //         return export
+    //         "#,
+    //     );
+    //     assert!(!ws.check_code_for(
+    //         DiagnosticCode::InjectField,
+    //         r#"
+    //         local a = require("a")
+    //         a.newField = 1
+    //         "#,
+    //     ));
+    //     assert!(ws.check_code_for(
+    //         DiagnosticCode::InjectField,
+    //         r#"
+    //         local a = require("a")
+    //         a.a = 2
+    //         "#,
+    //     ));
+    // }
 
-    #[test]
-    fn test_export_2() {
-        let mut ws = VirtualWorkspace::new();
-        ws.def_file(
-            "a.lua",
-            r#"
-            ---@export
-            return {
-                a = 1
-            }
-            "#,
-        );
-        assert!(!ws.check_code_for(
-            DiagnosticCode::InjectField,
-            r#"
-            local a = require("a")
-            a.newField = 1
-            "#,
-        ));
-        assert!(ws.check_code_for(
-            DiagnosticCode::InjectField,
-            r#"
-            local a = require("a")
-            a.a = 2
-            "#,
-        ));
-    }
+    // #[test]
+    // fn test_export_2() {
+    //     let mut ws = VirtualWorkspace::new();
+    //     ws.def_file(
+    //         "a.lua",
+    //         r#"
+    //         ---@export
+    //         return {
+    //             a = 1
+    //         }
+    //         "#,
+    //     );
+    //     assert!(!ws.check_code_for(
+    //         DiagnosticCode::InjectField,
+    //         r#"
+    //         local a = require("a")
+    //         a.newField = 1
+    //         "#,
+    //     ));
+    //     assert!(ws.check_code_for(
+    //         DiagnosticCode::InjectField,
+    //         r#"
+    //         local a = require("a")
+    //         a.a = 2
+    //         "#,
+    //     ));
+    // }
 
     #[test]
     fn test_issue_660() {
--- crates/emmylua_code_analysis/src/diagnostic/test/undefined_field_test.rs
+++ crates/emmylua_code_analysis/src/diagnostic/test/undefined_field_test.rs
@@ -736,31 +736,31 @@ mod test {
         ));
     }
 
-    #[test]
-    fn test_export() {
-        let mut ws = VirtualWorkspace::new();
-        ws.def_file(
-            "a.lua",
-            r#"
-            ---@export
-            local export = {}
+    // #[test]
+    // fn test_export() {
+    //     let mut ws = VirtualWorkspace::new();
+    //     ws.def_file(
+    //         "a.lua",
+    //         r#"
+    //         ---@export
+    //         local export = {}
 
-            return export
-            "#,
-        );
-        assert!(!ws.check_code_for(
-            DiagnosticCode::UndefinedField,
-            r#"
-            local a = require("a")
-            a.func()
-            "#,
-        ));
+    //         return export
+    //         "#,
+    //     );
+    //     assert!(!ws.check_code_for(
+    //         DiagnosticCode::UndefinedField,
+    //         r#"
+    //         local a = require("a")
+    //         a.func()
+    //         "#,
+    //     ));
 
-        assert!(!ws.check_code_for(
-            DiagnosticCode::UndefinedField,
-            r#"
-            local a = require("a").ABC
-            "#,
-        ));
-    }
+    //     assert!(!ws.check_code_for(
+    //         DiagnosticCode::UndefinedField,
+    //         r#"
+    //         local a = require("a").ABC
+    //         "#,
+    //     ));
+    // }
 }
